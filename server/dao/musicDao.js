12762463e763a75cbeef2d8c9566e1e4b5c68e09
avendesta <53615807+avendesta@users.noreply.github.com>
2022-03-14 12:58:56 -0500

    added sample data for database
    removed comments

--- server/dao/musicDao.js
+++ server/dao/musicDao.js
@@ -3,35 +3,29 @@ const { Music } = require('../models/music')
 // retrieve all music
 exports.getAll = async function(){
     const result = await Music.find({})
-    console.log("getAll: ",result)
     return result;
 }
 
 // retrieve a music by Userid
 exports.getByUserId = async function(userId){
     const result = await Music.find({userId: userId})
-    console.log("getByUserId: ",result)
     return result;
 }
 
 // retrieve a music by Musicid
 exports.getByMusicId = async function(musicId){
     const result = await Music.find({musicId: musicId})
-    console.log("getByMusicId: ",result)
     return result;
 }
 
 // add a music
 exports.addOne = async function(music){
     const result = await new Music(music).save()
-    console.log("addOne:",result)
     return true
 }
 
 // delete a music by id
 exports.removeOne = async function(id){
-    // db.music = db.music.filter(s => s.id != id)
-    console.log("delete Music:", id)
     await Music.deleteOne({_id: id})
     return true
 }
