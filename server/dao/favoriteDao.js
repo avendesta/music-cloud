e2cae741bdcab0b47bfe91c9f7cca48e61406dba
avendesta <53615807+avendesta@users.noreply.github.com>
2022-03-14 17:36:33 -0500

    fixed bug with favorites

--- server/dao/favoriteDao.js
+++ server/dao/favoriteDao.js
@@ -3,21 +3,21 @@ const Favorite = require('../models/favorite')
 // retrieve all favorite
 exports.getAll = async function(){
     const result = await Favorite.find({},{updatedAt: 0, __v: 0})
-    console.log("getAll: ",result)
+    // console.log("getAll: ",result)
     return result;
 }
 
 // retrieve a favorite by Userid
 exports.getByUserId = async function(userId){
-    const result = await Favorite.find({},{updatedAt: 0, __v: 0}).populate('music')
-    console.log("getByUserId: ",result)
+    const result = await Favorite.find({user:userId}).populate('music')
+    // console.log("getByUserId: ",result)
     return result;
 }
 
 // retrieve a favorite by Musicid
 exports.getByMusicId = async function(musicId){
-    const result = await Favorite.find({musicId: musicId},{updatedAt: 0, __v: 0})
-    console.log("getByMusicId: ",result)
+    const result = await Favorite.find({music: musicId})
+    // console.log("getByMusicId: ",result)
     return result;
 }
 
@@ -25,14 +25,14 @@ exports.getByMusicId = async function(musicId){
 exports.addOne = async function(body){
     const favorite = new Favorite(body)
     const result = await favorite.save()
-    console.log("addOne:",result)
+    // console.log("addOne:",result)
     return true
 }
 
 // delete a favorite by id
 exports.removeOne = async function(id){
     // db.favorite = db.favorite.filter(s => s.id != id)
-    console.log("delete Favorite:", id)
+    // console.log("delete Favorite:", id)
     await Favorite.deleteOne({_id: id})
     return true
 }
