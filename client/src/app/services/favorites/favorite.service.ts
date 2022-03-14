d44a0ef978a922b6d3c2a4c8bc7918d5a427bc5a
eliascanaza <sistemc44@gmail.com>
2022-03-14 14:56:04 -0500

    favorites pages added client side

---
+++ client/src/app/services/favorites/favorite.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteService {
+
+  constructor(private client: HttpClient) { }
+
+  //get all favorites from user
+  getAll(userId: String) {
+    return this.client.get('http://localhost:3000/favorite/user/' + userId)
+  }
+}
