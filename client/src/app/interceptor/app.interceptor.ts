fc4b98ef59873d56771d5e5698fabec93ad3a3fe
eliascanaza <sistemc44@gmail.com>
2022-03-15 17:34:34 -0500

    interceptor token default value added

--- client/src/app/interceptor/app.interceptor.ts
+++ client/src/app/interceptor/app.interceptor.ts
@@ -15,7 +15,7 @@ export class AppInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
-    let sessionToken = this.utilService.getSession()!
+    let sessionToken = this.utilService.getSession() || ''
     //if it is login
     if(request.url.includes("login") || request.url.includes("register")){
       return next.handle(request);
